9706f8712e37e21d18e24b4474c4bea55d4b4e61
Luciana Barbosa <luciheit@gmail.com>
2020-08-06 15:01:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let nome = prompt('Digite seu nome:');
+let nota1 = parseFloat(prompt('Digite a nota do 1º semestre:').replace(',','.'));
+let nota2 = parseFloat(prompt('Digite a nota do 2º semestre:').replace(',','.'));
+let nota3 = parseFloat(prompt('Digite a nota do 3º semestre:').replace(',','.'));
+let media = 7;
+let notasValidas = nota1 !== NaN && nota2 !== NaN && nota3 !== NaN;
+let mediaAluno;
+let alunoAprovado;
+let alunoReprovado;
+
+if (notasValidas) {
+  mediaAluno = (nota1 + nota2 + nota3) / 3;
+  alunoAprovado = mediaAluno >= media;
+  alunoReprovado = mediaAluno < media;
+  
+  if (alunoAprovado) {
+    alert (`Parabéns, ${nome}, você foi passou de ano com média ${mediaAluno.toFixed(0)}, aproveite suas férias!`);
+
+  } else if (alunoReprovado){
+      alert (`Que pena, ${nome}, sua média é ${mediaAluno.toFixed(0)} e você não obteve aprovação.`);
+
+      let pergunta = confirm('Gostaria de fazer recuperação?');
+      
+      if (pergunta) {
+        alert (`Ótimo, as aulas de recuperação começam semana que vem, ${nome}!`);
+      } else {
+          alert (`Que pena, te vejo ano que vem, ${nome}`);
+      }
+  }
+}
